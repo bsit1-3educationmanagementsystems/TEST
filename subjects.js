bff975607ab58456c52f7dfc45f5aabedcbe0a9d
bsit1-3educationmanagementsystems <bsiteducationmanagementsystems@gmail.com>
2025-11-26 18:24:02 +0800

    Update subjects.js

--- subjects.js
+++ subjects.js
@@ -1,3 +1,50 @@
+const searchbar = document.getElementById('searchbar');
+const easteregg = document.getElementById('EASTEREGG');
+const pics = document.getElementById('PICS');
+
+searchbar.addEventListener("keydown", function(event){
+        
+        if (event.key === "Enter"){
+            if (searchbar.value === "ezekiel"){
+                console.log('BISAYA KA EXZEL');
+                pics.style = "background-img:ezekiel.jpg;"
+            } else {}
+            if (searchbar.value === "jc"){
+            easteregg.innerHTML = "BISAYAAAAAAAAAAAA"
+            easteregg.style = "font-size:400px;"
+            }
+
+           if (
+            searchbar.value === "home" ||
+            searchbar.value === "main" ||
+            searchbar.value === "index"
+            ) {
+            window.location.href = "index.html";
+            }
+
+            if (
+            searchbar.value === "application" ||
+            searchbar.value === "sign-in" ||
+            searchbar.value === "student" ||
+            searchbar.value === "teacher" ||
+            searchbar.value === "users"
+            ) {
+            window.location.href = "application.html";
+            }
+
+            if (
+            searchbar.value === "subjects" ||
+            searchbar.value === "subject" ||
+            searchbar.value === "courses" ||
+            searchbar.value === "guest"
+            ) {
+            window.location.href = "sbjctGuest.html";
+        }
+
+  }})
+
+
+
 const role = document.body.dataset.role;
 // Subject 
 
@@ -195,3 +242,4 @@ subjectSelect.addEventListener("change", updateFileList);
 
 // Initial Load
 updateFileList();
+
