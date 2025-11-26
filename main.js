824ecce52998ddffeb3e8e7defb7e987b96f6db7
bsit1-3educationmanagementsystems <bsiteducationmanagementsystems@gmail.com>
2025-11-26 18:23:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,8 +12,35 @@ searchbar.addEventListener("keydown", function(event){
             if (searchbar.value === "jc"){
             easteregg.innerHTML = "BISAYAAAAAAAAAAAA"
             easteregg.style = "font-size:400px;"
-    
+            }
+
+           if (
+            searchbar.value === "home" ||
+            searchbar.value === "main" ||
+            searchbar.value === "index"
+            ) {
+            window.location.href = "index.html";
+            }
+
+            if (
+            searchbar.value === "application" ||
+            searchbar.value === "sign-in" ||
+            searchbar.value === "student" ||
+            searchbar.value === "teacher" ||
+            searchbar.value === "users"
+            ) {
+            window.location.href = "application.html";
+            }
+
+            if (
+            searchbar.value === "subjects" ||
+            searchbar.value === "subject" ||
+            searchbar.value === "courses" ||
+            searchbar.value === "guest"
+            ) {
+            window.location.href = "sbjctGuest.html";
         }
-    }}) 
+
+  }})
 
     //windows.location.href = "ss.html"
