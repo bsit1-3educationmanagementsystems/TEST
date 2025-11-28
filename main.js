a7fdad59ae0f37f6571aa22e70f42dacdee56b77
bsit1-3educationmanagementsystems <bsiteducationmanagementsystems@gmail.com>
2025-11-29 05:59:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,24 +5,6 @@ const pics = document.getElementById('PICS');
 searchbar.addEventListener("keydown", function(event){
         
         if (event.key === "Enter"){
-            if (searchbar.value === "ezekiel") {
-            pics.style.backgroundImage = "url('ezekiel.jpg')";
-            pics.style.backgroundSize = "cover";      
-            pics.style.backgroundPosition = "center"; 
-            }
-
-            if (searchbar.value === "clarence") {
-            pics.style.backgroundImage = "url('clarence.jpg')";
-            pics.style.backgroundSize = "cover";      
-            pics.style.backgroundPosition = "center"; 
-            }
-
-            } 
-            if (searchbar.value === "jc"){
-            easteregg.innerHTML = "BISAYAAAAAAAAAAAA"
-            easteregg.style = "font-size:400px;"
-            }
-
             if (
             searchbar.value === "home" ||
             searchbar.value === "Home" ||
@@ -50,9 +32,40 @@ searchbar.addEventListener("keydown", function(event){
             searchbar.value === "courses" ||
             searchbar.value === "guest"
             ) {
-            window.location.href = "sbjctGuest.html";
-        }
+            window.location.href = "sbjctGuestmain.html";
+            }
 
-  })
+            if (
+            searchbar.value === "science"
+            ) {
+            window.location.href = "sbjctGuest.html";
+            }
+
+            if (
+            searchbar.value === "math"
+            ) {
+            window.location.href = "sbjctGuestMath.html";
+            }
+
+            if (
+            searchbar.value === "english"
+            ) {
+            window.location.href = "sbjctGuestEng.html";
+            }
+
+            if (
+            searchbar.value === "reading"
+            ) {
+            window.location.href = "sbjctGuestRead.html";
+            }
+
+            if (
+            searchbar.value === "abstract"
+            ) {
+            window.location.href = "sbjctGuestAbs.html";
+            }
+
+  }})
+
 
     //windows.location.href = "ss.html"
